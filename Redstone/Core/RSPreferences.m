13607163718d71d6c0b200468ff2e3b5a3be3ed0
SniperGER <janikschmidt@icloud.com>
2017-08-25 12:34:54 +0200

    Cleanup

--- Redstone/Core/RSPreferences.m
+++ Redstone/Core/RSPreferences.m
@@ -12,54 +12,6 @@ static RSPreferences* sharedInstance;
 	if (self = [super init]) {
 		sharedInstance = self;
 		
-		/*preferences = [NSMutableDictionary dictionaryWithContentsOfFile:PREFERENCES_PATH];
-		
-		if (!preferences) {
-			preferences = [NSMutableDictionary new];
-		}
-		
-		// Main Switch
-		if (![preferences objectForKey:@"enabled"]) {
-			[preferences setValue:[NSNumber numberWithBool:YES] forKey:@"enabled"];
-		}
-		
-		// Home Screen
-		if (![preferences objectForKey:@"homeScreenEnabled"]) {
-			[preferences setValue:[NSNumber numberWithBool:YES] forKey:@"homeScreenEnabled"];
-		}
-		
-		// Volume Controls
-		if (![preferences objectForKey:@"volumeControlsEnabled"]) {
-			[preferences setValue:[NSNumber numberWithBool:YES] forKey:@"volumeControlsEnabled"];
-		}
-		
-		// Accent Color
-		if (![preferences objectForKey:@"accentColor"]) {
-			[preferences setValue:@"#0078D7" forKey:@"accentColor"];
-		}
-		
-		// Tile Opacity
-		if (![preferences objectForKey:@"tileOpacity"]) {
-			[preferences setValue:[NSNumber numberWithFloat:0.8] forKey:@"tileOpacity"];
-		}
-		
-		// Columns
-		if (![preferences objectForKey:@"showMoreTiles"]) {
-			[preferences setValue:[NSNumber numberWithBool:YES] forKey:@"showMoreTiles"];
-		}
-		
-		// Default tile layout - 2 columns
-		if (![preferences objectForKey:@"2ColumnLayout"]) {
-			[preferences setObject:[NSArray arrayWithContentsOfFile:[NSString stringWithFormat:@"%@/2ColumnDefaultLayout.plist", RESOURCES_PATH]] forKey:@"2ColumnLayout"];
-		}
-			 
-		// Default tile layout - 3 columns
-		if (![preferences objectForKey:@"3ColumnLayout"]) {
-			[preferences setObject:[NSArray arrayWithContentsOfFile:[NSString stringWithFormat:@"%@/3ColumnDefaultLayout.plist", RESOURCES_PATH]] forKey:@"3ColumnLayout"];
-		}
-		
-		[preferences writeToFile:PREFERENCES_PATH atomically:YES];*/
-		
 		[self loadPreferences];
 		
 		if (!preferences) {
